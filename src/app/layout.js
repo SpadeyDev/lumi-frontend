616da5f0a7306368eec0858a91f0ad5df8c66084
Harun <spadeycontent@gmail.com>
2024-09-06 15:23:58 +0300

    feat:grid

--- src/app/layout.js
+++ src/app/layout.js
@@ -17,14 +17,16 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={poppins.className}>
-        <div className="flex justify-center ">
-          <div className="w-[1200px]">
-            <div className="flex items-center justify-center relative z-0">
-              <div className="w-64 h-32 blur-[200px] bg-[#4B4EE7] absolute "></div>
+        <div className="absolute inset-0 -z-1 h-[80vh] w-full mx-auto bg-white bg-[linear-gradient(to_right,#f0f0f0_1px,transparent_1px),linear-gradient(to_bottom,#f0f0f0_1px,transparent_1px)] bg-[size:6rem_4rem]">
+          <div className="flex justify-center ">
+            <div className="w-[1200px]">
+              <div className="flex items-center justify-center relative z-0">
+                <div className="w-64 h-32 blur-[200px] bg-[#4B4EE7] absolute "></div>
+              </div>
+              <Header />
+              {children}
+              <Footer />
             </div>
-            <Header />
-            {children}
-            <Footer />
           </div>
         </div>
       </body>
